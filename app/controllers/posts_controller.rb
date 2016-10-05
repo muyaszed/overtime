b90f7665a516ccf3835d7211995ff358a41812b7
muyaszed <muyaszed@gmail.com>
2016-10-05 13:33:39 +0100

    test post creation and refactor

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,36 @@
 class PostsController < ApplicationController
+	before_action :find_post, only: [:show]
+
 	def index
 
+	end
+
+	def new 
+		@post = Post.new
+	end
+
+	def show
+
+		
+	end
+
+	def create
+		@post = Post.new(post_params)
+
+		if @post.save
+			redirect_to @post, notice: 'Your post created successfully'
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:date, :rationale)
+	end
+
+	def find_post
+		@post = Post.find(params[:id])
 	end
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,14 +2,37 @@ require 'rails_helper'
 
 describe 'navigate' do
   context 'index' do
-  	it "can be reached succesfully" do
+  	before do
   		visit posts_path
+  	end
+
+  	it "can be reached succesfully" do
+  		
   		expect(page.status_code).to eq(200)  
   	end 
 
   	it "has a title post" do
-  		visit posts_path
+  		  
   		expect(page).to have_content(/Post/)   
+  	end  
+  end
+ 
+  context "create new post" do
+  	before do
+  		visit new_post_path
+  	end
+  	it "can be reached" do
+  		 
+  		expect(page.status_code).to eq(200)   
+  	end    
+
+  	it "can be created with new form" do
+  		
+  		fill_in 'post[date]', with: Date.today
+  		fill_in 'post[rationale]', with: "rationale"
+  		click_on "Save"
+ 
+  		expect(page).to have_content("rationale")   
   	end 
   end
 end 
