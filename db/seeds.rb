6ffecf86084a564669d73bdd9438a34b45c3f8d4
muyaszed <muyaszed@gmail.com>
2016-10-26 09:29:54 +0100

    modified seed data to include user relationship with post

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+user = User.create(email: "test@test.com", password: "12345678", password_confirmation: "12345678", first_name: "Yazed", last_name: "jamal")
+
 100.times do |post|
-	Post.create!(date: Date.today, rationale: "#{post} rational content")
+	Post.create!(date: Date.today, rationale: "#{post} rational content", user_id: user.id)
 end
