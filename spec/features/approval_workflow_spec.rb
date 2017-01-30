f786b7b8bc4b1ddd3873272db4b3a7e1e79f13c4
yazed jamal <muyaszed@gmail.com>
2017-01-30 11:08:35 +0000

    integrated non admin not to see post status

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -9,13 +9,24 @@ context 'navigate' do
 	describe 'edit' do
 		before do
 			@post = FactoryGirl.create(:post)
+			visit edit_post_path(@post)
 		end
 
 		it 'has a status that cab edited on the form' do
-			visit edit_post_path(@post)
+			
 			 choose "approved" 
 			 click_on "Save"
 			 expect(@post.reload.status).to eq('approved')  
+
+		end
+
+		it 'cannot be edited by non-admin' do
+			logout(:user)
+			user = FactoryGirl.create(:user)
+			login_as(user, :scope => :user)
+
+			visit edit_post_path(@post)
+			expect(page).to_not have_content('Approved')
 
 		end
 	end
