00e1fd92765d9ec88069801c78164c877c917317
Yazed Jamal <muyaszed@gmail.com>
2017-05-07 16:42:09 +0100

    refactored post spec file

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
 describe 'navigate post' do
+  
+  let(:user) {FactoryGirl.create(:user)}
+
+  let(:post) do
+    Post.create(date: Date.today, rationale: "user1 rationale", user_id: user.id)
+  end
+
   before do
-      @user = FactoryGirl.create(:user) 
-      login_as(@user, :scope => :user)  
+     
+      login_as(user, :scope => :user)  
       
   end
   context 'view all post' do
@@ -31,8 +38,7 @@ describe 'navigate post' do
     end   
 
     it 'has a scope so that only post from creators can see thier post' do
-      post1 = Post.create(date: Date.today, rationale: "asdf", user_id: @user.id)
-      post2 = Post.create(date: Date.today, rationale: "asdf", user_id: @user.id)
+      
 
       other_user = User.create(first_name: 'Non', last_name: 'Authorized', email: "nonauth@example.com", password: "asdfasdf", password_confirmation: "asdfasdf")
       post_from_other_user = Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id)
@@ -40,12 +46,9 @@ describe 'navigate post' do
       visit posts_path
 
       expect(page).to_not have_content(/This post shouldn't be seen/)
-      post_user1 = Post.create(date: Date.today, rationale: "rationale 1", user_id: @user.id)
-      user2 = FactoryGirl.create(:other_user)
-      post_user2 = Post.create(date: Date.today, rationale: "rationale 2", user_id: user2.id)
+    
       
-       visit posts_path 
-      expect(page).not_to have_content(/rationale 2/)
+       
     end
   end
  
@@ -80,19 +83,12 @@ describe 'navigate post' do
   end
 
   context "edit post" do
-    before do
-
-      @post = Post.create(date: Date.today, rationale: "1234", user_id: @user.id)
-       # @post = FactoryGirl.create(:post)
-      # visit posts_path
-    end
-
+    
     
 
     it "can be edited" do 
-      # byebug
-      visit edit_post_path(@post)
-      # expect(page).to have_content("Updated rationale")
+      
+      visit edit_post_path(post)
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Updated rationale"
       click_on 'Save'
@@ -104,16 +100,14 @@ describe 'navigate post' do
       logout(:user)
       non_authorized_user = FactoryGirl.create(:non_authorized_user) 
       login_as(non_authorized_user, :scope => :user)
-      visit edit_post_path(@post)
+      visit edit_post_path(post)
       expect(current_path).to eq(root_path) 
     end
   end
 
   context "delete post" do
     it "can be deleted by clicking delete link" do
-      @post = FactoryGirl.create(:post)
-      # TODO refactor
-      @post.update(user_id: @user.id)
+      @post = FactoryGirl.create(:post, user_id: user.id)
       visit posts_path
       click_link("delete_#{@post.id}")
       expect(page.status_code).to eq(200)    
